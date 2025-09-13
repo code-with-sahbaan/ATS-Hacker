93eb7f477598081efb6efe3a4c17c22f7095fa99
Sahbaan.Alam <sahbaan.alam.inc@gmail.com>
2025-09-13 13:47:01 +0500

    Frontend: Google Sign In flow added. — Backend: None.

--- Frontend/src/app/pages/sign-in/sign-in.ts
+++ Frontend/src/app/pages/sign-in/sign-in.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { CubesAnimation } from '../../components/cubes-animation/cubes-animation';
+import { environment } from '../../../environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [ButtonModule, CubesAnimation],
+  imports: [ButtonModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css'
 })
 export class SignIn {
+
+  constructor(private route: ActivatedRoute) { }
+
+  continueWithGoogle() {
+    window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + environment.redirectURL + "&response_type=code&client_id=" + environment.clientId + "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&access_type=offline"
+  }
+
+  ngOnInit(): void {
+    // Route parameters (e.g., /users/:id)
+    this.route.params.subscribe(params => {
+      console.log('Route Params:', params); // e.g., { id: '123' }
+    });
+
+    // Query parameters (e.g., ?search=test)
+    this.route.queryParams.subscribe(queryParams => {
+      console.log('Query Params:', queryParams); // e.g., { search: 'test' }
+    });
+  }
 
 }
