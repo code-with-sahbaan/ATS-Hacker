9166ce16720ef3636538c048736e6601546d2a8a
Sahbaan.Alam <sahbaan.alam.inc@gmail.com>
2025-10-03 22:42:36 +0500

    Frontend: None. — Backend: Methods added for getting reply of speech and speech to text.

--- Backend/BusinessModule/src/main/java/code/with/sahbaan/neohire/Services/AiInterviewService.java
+++ Backend/BusinessModule/src/main/java/code/with/sahbaan/neohire/Services/AiInterviewService.java
@@ -1,8 +1,11 @@
 package code.with.sahbaan.neohire.Services;
 
 import code.with.sahbaan.neohire.RequestDTO.Candidate.InitiateInterview;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface AiInterviewService {
 
     byte[] initiateInterview(InitiateInterview initiateInterview) throws Exception;
+
+    byte[] getReply(MultipartFile speech) throws Exception;
 }
--- Backend/BusinessModule/src/main/java/code/with/sahbaan/neohire/ServicesImpl/AiInterviewServiceImpl.java
+++ Backend/BusinessModule/src/main/java/code/with/sahbaan/neohire/ServicesImpl/AiInterviewServiceImpl.java
@@ -4,14 +4,19 @@ import code.with.sahbaan.neohire.RequestDTO.Candidate.InitiateInterview;
 import code.with.sahbaan.neohire.Services.AiInterviewService;
 import code.with.sahbaan.neohire.Services.UserService;
 import code.with.sahbaan.neohire.Utils.InterviewDetails;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.ai.openai.audio.speech.SpeechModel;
 import org.springframework.ai.openai.audio.speech.SpeechPrompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -28,6 +33,9 @@ public class AiInterviewServiceImpl implements AiInterviewService {
 
     @Autowired
     private SpeechModel speechModel;
+
+    @Autowired
+    private OpenAiAudioTranscriptionModel audioTranscriptionModel;
 
     private final Map<String, InterviewDetails> sessions = new ConcurrentHashMap<>();
 
@@ -47,7 +55,8 @@ public class AiInterviewServiceImpl implements AiInterviewService {
                     .append(initiateInterview.getApplyingForPosition())
                     .append(" that requires ")
                     .append(initiateInterview.getRequiredExperienceForJob())
-                    .append("of experience. You need to ask relevant queries and evaluate the candidate between 1-100");
+                    .append("of experience. You need to ask relevant queries and evaluate the candidate between 1-100.")
+                    .append(" Whenever user answer the query completely then ask next question or if he/she ask for some clarification of current one then give that.");
 
             // Building Introduction Message
             String query = "Introduce yourself to the candidate and ask for his introduction";
@@ -66,10 +75,48 @@ public class AiInterviewServiceImpl implements AiInterviewService {
             sessions.put(userService.getToken(), interviewDetails);
 
             // converting text into speech
-            return speechModel.call(new SpeechPrompt(response)).getResult().getOutput();
+            return textToSpeech(response);
 
         }catch(Exception e){
             throw new  Exception("Failed to initiate interview");
+        }
+    }
+
+    private byte[] textToSpeech(String message){
+        return speechModel.call(new SpeechPrompt(message)).getResult().getOutput();
+    }
+
+    private String speechToText(MultipartFile speech){
+        OpenAiAudioTranscriptionOptions aiAudioTranscriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .temperature(0f)
+                .build();
+        AudioTranscriptionPrompt audioTranscriptionPrompt = new AudioTranscriptionPrompt(speech.getResource(), aiAudioTranscriptionOptions);
+        return audioTranscriptionModel.call(audioTranscriptionPrompt).getResult().getOutput();
+    }
+
+    @Override
+    public byte[] getReply(MultipartFile speech) throws Exception {
+        try{
+            InterviewDetails interviewDetails = sessions.get(userService.getToken());
+            List<Message> messages = interviewDetails.getMessages();
+            String transcription = speechToText(speech);
+            String response = chatClient.defaultSystem(interviewDetails
+                    .getSystemMessage())
+                    .build()
+                    .prompt()
+                    .user(transcription)
+                    .messages(messages)
+                    .call()
+                    .content();
+
+            messages.add(new UserMessage(transcription));
+            messages.add(new AssistantMessage(response));
+            interviewDetails.setMessages(messages);
+            sessions.put(userService.getToken(), interviewDetails);
+            return textToSpeech(response);
+        }catch(Exception e){
+            throw new  Exception("Failed to get reply");
         }
     }
 }
